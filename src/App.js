d205352276e4973ca181417248011233f394174a
Emma Clarke <emmacodeswell@gmail.com>
2023-10-26 21:05:05 -0400

    Import and create `states`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
+const initialState = {
+  bill: 0,
+  tip: 0,
+  people: 0
+}
+
 function App() {
+  const [userInput, setuserInput] = useState(initialState)
+  const [tip, setTip] = useState(0)
+  const [total, setTotal] = useState(0)
+  
   return (
     <div>
     </div>
