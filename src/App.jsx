824df69c54c662e222b55d7e8dc5b8abc4520d93
Emma Clarke <emmacodeswell@gmail.com>
2023-11-01 16:32:20 -0400

    Create inputs that can be reset on reset button

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import formatter from "./helpers/formatter"
 
 const stateSetter = setState => event => setState(event.target.value)
 
+const isDefined = value => value !== undefined
+
 function App() {
-  const [numPeople, setNumPeople] = useState(0)
-  const [tip, setTip] = useState(0)
-  const [bill, setBill] = useState(0)
+  const [numPeople, setNumPeople] = useState('')
+  const [tip, setTip] = useState('')
+  const [bill, setBill] = useState('')
 
   const handleTip = stateSetter(setTip)
   const handleBill = stateSetter(setBill)
   const handleNumPeople = stateSetter(setNumPeople)
+
+  const handleRadioTip = event => {
+    event.currentTarget.form.elements.customTip.value = ''
+    handleTip(event)
+  }
+
+  const handleCustomTip = event => {
+    event.currentTarget.form.elements.tip.forEach(input => {
+      input.checked = false
+    })
+
+    handleTip(event)
+  }
+
   let formattedTip
   let formattedTotal
 
@@ -24,41 +40,49 @@ function App() {
     formattedTotal = formatter.format(totalPerPerson)
   }
 
+  const resetState = event => {
+    setTip()
+    setNumPeople()
+    setBill()
+  }
+
   return (
     <div>
       <h1>
         <img src={logo} alt="Splitter logo" />
       </h1>
-      <label>
-        Bill
-        <input type="number" onChange={handleBill}/>
-      </label>
-      <label>
-        Select Tip %
-        <input type="radio" name="tip" value="5" onChange={handleTip}/>
-        <input type="radio" name="tip" value="10" onChange={handleTip}/>
-        <input type="radio" name="tip" value="15" onChange={handleTip}/>
-        <input type="radio" name="tip" value="25" onChange={handleTip}/>
-        <input type="radio" name="tip" value="50" onChange={handleTip}/>
-        <input type="number" onChange={handleTip}/>
-      </label>
-      <label>
-        Number of People
-        <input type="number" onChange={handleNumPeople}/>
-      </label>
-      <div className="results">
+      <form onReset={resetState}>
         <label>
-          Tip Amount
-          <small>/ person</small>
-          <output>{tipPerPerson}</output>
+          Bill
+          <input type="number" onChange={handleBill}/>
         </label>
         <label>
-          Total
-          <small>/ person</small>
-          <output>{totalPerPerson}</output>
+          Select Tip %
+          <input type="radio" name="tip" value="5" onChange={handleRadioTip}/>
+          <input type="radio" name="tip" value="10" onChange={handleRadioTip}/>
+          <input type="radio" name="tip" value="15" onChange={handleRadioTip}/>
+          <input type="radio" name="tip" value="25" onChange={handleRadioTip}/>
+          <input type="radio" name="tip" value="50" onChange={handleRadioTip}/>
+          <input type="number" name="customTip" placeholder="Custom" onChange={handleCustomTip}/>
         </label>
-      </div>
-      <input type="reset" />
+        <label>
+          Number of People
+          <input type="number" onChange={handleNumPeople}/>
+        </label>
+        <div className="results">
+          <label>
+            Tip Amount
+            <small>/ person</small>
+            <output>{formattedTip}</output>
+          </label>
+          <label>
+            Total
+            <small>/ person</small>
+            <output>{formattedTotal}</output>
+          </label>
+        </div>
+        <input type="reset" />
+      </form>
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
         Coded by <a href="https://github.com/emmacodeswell" target="_blank">Emma Clarke</a>.
