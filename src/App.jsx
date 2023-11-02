de0a958438f253cda193d1f8fc284964f0bad6ef
Emma Clarke <emmacodeswell@gmail.com>
2023-11-01 22:30:37 -0400

    Add markup for styling

--- src/App.jsx
+++ src/App.jsx
@@ -51,42 +51,54 @@ function App() {
       <h1>
         <img src={logo} alt="Splitter logo" />
       </h1>
-      <form onReset={resetState}>
-        <label>
-          Bill
-          <input type="number" onChange={handleBill}/>
-        </label>
-        <label>
-          Select Tip %
-          <input type="radio" name="tip" value="5" onChange={handleRadioTip}/>
-          <input type="radio" name="tip" value="10" onChange={handleRadioTip}/>
-          <input type="radio" name="tip" value="15" onChange={handleRadioTip}/>
-          <input type="radio" name="tip" value="25" onChange={handleRadioTip}/>
-          <input type="radio" name="tip" value="50" onChange={handleRadioTip}/>
-          <input type="number" name="customTip" placeholder="Custom" onChange={handleCustomTip}/>
-        </label>
-        <label>
-          Number of People
-          <input type="number" onChange={handleNumPeople}/>
-        </label>
-        <div className="results">
-          <label>
-            Tip Amount
-            <small>/ person</small>
-            <output>{formattedTip}</output>
-          </label>
-          <label>
-            Total
-            <small>/ person</small>
-            <output>{formattedTotal}</output>
-          </label>
-        </div>
-        <input type="reset" />
-      </form>
-      <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="https://github.com/emmacodeswell" target="_blank">Emma Clarke</a>.
+      <div className="wrapper">
+        <form onReset={resetState}>
+          <div className="inputs">
+            <label>
+              Bill
+              <input className="bill" type="number" onChange={handleBill} placeholder="0"/>
+            </label>
+            <div className="tips">
+              <label>
+                Select Tip %
+                <input type="radio" name="tip" value="5" onChange={handleRadioTip} />
+                <input type="radio" name="tip" value="10" onChange={handleRadioTip} />
+                <input type="radio" name="tip" value="15" onChange={handleRadioTip} />
+                <input type="radio" name="tip" value="25" onChange={handleRadioTip} />
+                <input type="radio" name="tip" value="50" onChange={handleRadioTip} />
+                <input type="number" name="customTip" placeholder="Custom" onChange={handleCustomTip} />
+              </label>
+            </div>
+            <label>
+              Number of People
+              <input className="numPeople" type="number" onChange={handleNumPeople} placeholder="0"/>
+            </label>
+          </div>
+          <div className="results">
+            <div className="total">
+              <label>
+                Tip Amount
+                <br />
+                <small>/ person</small>
+              </label>
+              <output>{formattedTip}</output>
+            </div>
+            <div className="total">
+              <label>
+                Total
+                <br />
+                <small>/ person</small>
+              </label>
+              <output>{formattedTotal}</output>
+            </div>
+            <input className="reset" type="reset" />
+          </div>
+        </form>
       </div>
+      <footer>
+        Design by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/emmacodeswell" target="_blank">Emma Clarke</a> in 2023 💙.
+      </footer>
     </div>
   );
 }
